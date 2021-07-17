008adbf9beaa548468bbc6179c10f727ec5fe932
Antti <antti@antti.codes>
2021-07-17 12:08:31 +0300

    fix: settings ensuring

--- src/db.js
+++ src/db.js
@@ -1,12 +1,10 @@
 import Josh from "@joshdb/core";
 import provider from "@joshdb/sqlite";
-import config from "./config.js";
 
 // Export the database for guild settings
 export const settings = new Josh({
     name: "settings",
     provider,
-    autoEnsure: config.defaultSettings,
 });
 
 // Export a seperate database for RSS feeds
--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,8 +1,12 @@
 import button from "../interactions/button.js";
 import command from "../interactions/command.js";
 import selectMenu from "../interactions/selectMenu.js";
+import config from "../config.js";
+import { settings } from "../db.js";
 
-export default (client, interaction) => {
+export default async (client, interaction) => {
+    // Ensure settings manually as autoEnsure is broken
+    if (interaction.inGuild()) await settings.ensure(interaction.guildId, config.defaultSettings);
     // Check what type of interaction it is and route it to the correct handler
     if (interaction.isCommand()) command(client, interaction);
     else if (interaction.isButton()) button(client, interaction);
