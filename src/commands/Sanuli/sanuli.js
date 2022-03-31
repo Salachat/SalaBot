373b241d6ceebc6f272d695c6e229810b92f287d
Antti <antti@antti.codes>
2022-03-31 20:33:24 +0300

    fix: sanuli stuff

--- src/commands/Sanuli/sanuli.js
+++ src/commands/Sanuli/sanuli.js
@@ -44,49 +44,73 @@ export default {
                     guesses: {},
                 });
                 const data = await sanulit.get(user);
-                const guesses = Object.values(data.guesses);
+                const playedGames = Object.keys(data.guesses).length;
+                const guesses = playedGames
+                    ? Array(
+                          Object.keys(data.guesses)
+                              .map(Number)
+                              .sort((a, b) => b - a)[0] + 1
+                      )
+                          .fill(0)
+                          .map((v, i) => data.guesses[i] ?? v)
+                    : [];
                 const stats = guesses.reduce((g, n) => {
                     const k = n.toString();
                     g[k] = g[k] ? g[k] + 1 : 1;
                     return g;
                 }, {});
+                const totalStat = Array(6)
+                    .fill()
+                    .map((_, i) => stats[i + 1] ?? 0)
+                    .reduce((a, b) => a + b);
+                const wonGames = guesses.filter((n) => n > 0);
                 const embed = new MessageEmbed()
                     .setTitle(`Sanulis of ${command.user.tag}`)
                     .setColor("#F4AFAB")
                     .setDescription(
                         "**Statistics**" +
-                            `\n__Played__: ${guesses.length}` +
+                            `\n__Played__: ${playedGames}` +
                             `\n__Win-%__: ${
-                                guesses.length
-                                    ? Math.round(
-                                          (guesses.length / guesses.filter((n) => n > 0).length) *
-                                              100
-                                      )
-                                    : 0
+                                playedGames ? Math.round((wonGames.length / playedGames) * 100) : 0
                             } %` +
-                            `\n__Streak__: ${guesses.length - 1 - guesses.lastIndexOf(-1)}` +
+                            `\n__Streak__: ${
+                                guesses.length -
+                                1 -
+                                Math.min(guesses.lastIndexOf(-1), guesses.lastIndexOf(0))
+                            }` +
                             `\n__Best streak__: ${guesses.reduce((best, _, index, arr) => {
-                                let streak = arr.slice(index).indexOf(-1);
+                                const part = arr.slice(index);
+                                let streak = Math.min(part.indexOf(-1), part.indexOf(0));
                                 streak = streak === -1 ? arr.length - index : streak;
                                 return streak > best ? streak : best;
                             }, 0)}` +
                             `\n__Average guesses__: ${
-                                guesses.length
-                                    ? Math.round(
-                                          guesses
-                                              .filter((n) => n !== -1)
-                                              .reduce((a, n) => a + n, 0) / guesses.length
-                                      )
+                                playedGames
+                                    ? (
+                                          wonGames.reduce((a, n) => a + n, 0) / wonGames.length
+                                      ).toFixed(2)
                                     : 0
                             }` +
                             "\n" +
                             "\n**Distribution**" +
-                            `\n**1:** ${stats["1"] ?? 0}` +
-                            `\n**2:** ${stats["2"] ?? 0}` +
-                            `\n**3:** ${stats["3"] ?? 0}` +
-                            `\n**4:** ${stats["4"] ?? 0}` +
-                            `\n**5:** ${stats["5"] ?? 0}` +
-                            `\n**6:** ${stats["6"] ?? 0}`
+                            `\n\`1: ${stats["1"] ?? 0}\` ${"🟩".repeat(
+                                Math.floor((totalStat / stats["1"]) * 11)
+                            )}` +
+                            `\n\`2: ${stats["2"] ?? 0}\` ${"🟩".repeat(
+                                Math.floor((totalStat / stats["2"]) * 11)
+                            )}` +
+                            `\n\`3: ${stats["3"] ?? 0}\` ${"🟩".repeat(
+                                Math.floor((totalStat / stats["3"]) * 11)
+                            )}` +
+                            `\n\`4: ${stats["4"] ?? 0}\` ${"🟩".repeat(
+                                Math.floor((totalStat / stats["4"]) * 11)
+                            )}` +
+                            `\n\`5: ${stats["5"] ?? 0}\` ${"🟩".repeat(
+                                Math.floor((totalStat / stats["5"]) * 11)
+                            )}` +
+                            `\n\`6: ${stats["6"] ?? 0}\` ${"🟩".repeat(
+                                Math.floor((totalStat / stats["6"]) * 11)
+                            )}`
                     );
                 await command.editReply({ embeds: [embed] });
                 break;
@@ -102,8 +126,8 @@ export default {
                                   .reduce((a, n) => a + n, 0) / Object.keys(u.guesses).length
                             : 0,
                     }))
-                    .slice(0, 10)
-                    .sort((a, b) => b.total - a.total);
+                    .sort((a, b) => b.total - a.total)
+                    .slice(0, 10);
                 const embed = new MessageEmbed()
                     .setTitle("Sanuli Leaderboard")
                     .setColor("#F4AFAB")
